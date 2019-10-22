098e2a5be00326ac893474d9169c742794aa7fa2
Ronnie Goodrich <kahootbird@gmail.com>
2019-10-22 17:33:43 -0400

    Text animating properly after search

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import reddit from './reddit_api'
-
 //Word array sample
-var txt_arr = ["word1", "word2", "word3", "word4"]
-
+/*
+//sample data
+var txt_arr = ["0 0 0 0 0 0 0 0 0 0",
+               "1 1 1 1 1 1 1 1 1 1",
+               "2 2 2 2 2 2 2 2 2 2"]
+*/
+var txt_arr = []
+var word_str = ""
+var line_num = 0
+//Start at the current line
+var current_line
+var word_index = 0
+var interval_set = 0
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 searchForm.addEventListener('submit', e => {
   const searchTerm = searchInput.value;
-
   const sortBy = document.querySelector('input[name="sortby"]:checked').value
-
   //Check input
   if (searchTerm == '') {
     showMessage('Please add a search term', 'alert-danger')
@@ -17,17 +25,18 @@ searchForm.addEventListener('submit', e => {
   //const searchLimit = document.getElementById('limit').value;
   const searchLimit = 10
   console.log(searchLimit)
-txt_arr = []
+
+//txt_arr = []
   reddit.search(searchTerm, searchLimit, sortBy)
   .then(results => {
     //console.log(results)
     let output = '<div class="card-columns">'
     results.forEach(post => {
 //console.log(post)
-//txt_arr.push(post.selftext)
-txt_arr.push("This is a text message that is very long aaa bbb ccc ddd eee fff ggg hhh iii jjj kkk")
-
-console.log(txt_arr )
+txt_arr.push(post.selftext)
+//txt_arr.push("This is a text message that is very long aaa bbb ccc ddd eee fff ggg hhh iii jjj kkk")
+word_str = txt_arr[txt_arr.length-1].split(" ")
+console.log(txt_arr[txt_arr.length-1].split(" "))
 
       //let image = post.preview ? post.preview.images[0].source.url : img_txt
 
@@ -58,14 +67,51 @@ let img_txt = 'https://www.redditstatic.com/icon.png'
     document.getElementById('results').innerHTML = output;
 
   });
+  current_line = 0
   e.preventDefault();
+  console.log("TXT ARRAY")
+  console.log(word_str)
+  console.log(txt_arr.length)
+  word_index = 0
 
+  if (interval_set == 0)
+  {
+  setInterval(
+    function(){ flash_words(); },300
+  )
+    interval_set = 1
+  }
 })
 
+function flash_words()
+{
+  //Move to the next line if the end is reached and set to start at word 0
+  //console.log("LIN" + current_line + " " + txt_arr.length)
+    if (word_index == word_str.length && current_line != txt_arr.length)
+    {
+      current_line++
+      console.log("RESET")
+      //console.log("RAN" + word_str.length + " " + current_line + " " + word_str.length)
+      word_index = 0
+      word_str = txt_arr[current_line].split(" ")
+      console.log("INDX" + word_index + " " + word_str.length)
+
+    }
+    //Show the next word if the end hasn't been reached.
+
+    console.log(txt_arr[current_line])
+    if (word_index < word_str.length)
+    {
+      set_word(word_str[word_index])
+      word_index++
+    }
+}
+
 //Hide the search button
-hide_search_gui();
+//hide_search_gui();
+
 //Set speed read word
-set_word("a");
+set_word(" ");
 function set_words()
 {
 
