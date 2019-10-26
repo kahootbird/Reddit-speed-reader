45ed27e6b54793abcb0c3405ed136fbf2811100f
Ronnie Goodrich <kahootbird@gmail.com>
2019-10-26 00:28:44 -0400

    another attempted fix

--- reddit_api.js
+++ reddit_api.js
@@ -1,7 +1,7 @@
 export default {
 		search: function(searchTerm, searchLimit, sortBy)
 		{
-			return fetch(`//www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+			return fetch(`https://www.reddit.com/r/secure/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
 			.then(res => res.json())
 			.then(data => data.data.children.map(data => data.data))
 			.catch(err => console.log(err));
